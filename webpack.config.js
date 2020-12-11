c0fc6c5b996da17b3cb09d05251ca7b6d520f19c
Nade <s.nadjufka@gmail.com>
2020-12-11 23:58:24 +0300

    Проектная работа №9 fix for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-        publicPath: '',
+        publicPath: '/mesto/',
     },
     mode: "development",
     devServer: {
@@ -49,6 +49,5 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin(),
-
     ]
 }
